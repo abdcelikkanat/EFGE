ee7e23d15b4d73503f52acac88cfe24533e2b190
Abdulkadir Celikkanat <abdcelikkanat@gmail.com>
2019-05-17 11:48:39 +0200

    lab commit

--- main.cpp
+++ main.cpp
@@ -38,15 +38,9 @@ int main(int argc, char** argv) {
     int window_size = 10;
     int negative_sample_size = 5;
     int dim = 128;
-<<<<<<< HEAD
-    double starting_alpha = 0.025*0.1;
-    double decay_rate = 1.0;
-    double min_alpha = 0.0001*0.1;
-=======
     double starting_alpha = 0.025;
-    double decay_rate = 1;
+    double decay_rate = 1.0;
     double min_alpha = 0.0001;
->>>>>>> 8e002907943e6d61a27682fa34987ece5ccfc46b
     int num_iters = 1;
 
 
